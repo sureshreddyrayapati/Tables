3e378027d7ba791542b4ae08fe1e31f36412b091
sureshreddyrayapati <resuresh0@gmail.com>
2024-02-15 13:01:34 +0530

    Add project files.

---
+++ Tables/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome to 5 Table");
+int n = 5;
+for (int i = 1; i <= n; i++)
+{
+    Console.WriteLine("{0} * {1} = {2}", 5, i, (n * i));
+}
+
